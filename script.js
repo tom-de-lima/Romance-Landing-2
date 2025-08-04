373340d1cf13322219a5ac952e38917057ff8759
Tom de Lima <antoniojosedelimaneto@gmail.com>
2025-08-04 16:38:30 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+/* script.js */
+document.getElementById("play-button").addEventListener("click", function () {
+  var audio = document.getElementById("background-music")
+  audio.play()
+  this.disabled = true // desabilita o botão após clique
+})
+
+window.addEventListener("scroll", function () {
+  var finalImage = document.getElementById("final-image")
+  if (
+    window.innerHeight + window.pageYOffset >=
+    document.body.offsetHeight - 10
+  ) {
+    finalImage.style.opacity = 1
+  }
+})
